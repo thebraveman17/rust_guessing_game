50cfd4a62e45eb749cb339755c6bb48fb9ee9a24
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-01-29 16:45:06 +0100

    Take user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut user_guess = String::new();
+
+    io::stdin()
+        .read_line(&mut user_guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", user_guess);
 }
