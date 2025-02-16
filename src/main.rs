2a13ff509d1d3c47f8f5af80f3c1ecca990fb459
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-02-16 11:58:59 +0100

    Take user input for range and print number of attempts

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,37 @@
 use rand::Rng;
 use std::cmp::Ordering;
-use std::io;
+use std::io::{self};
 
 fn main() {
     println!("Guess the number!");
+    println!("Please input the upper limit of your desired range:");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let upper_limit: u32 = loop {
+        let mut upper_limit = String::new();
+
+        io::stdin()
+            .read_line(&mut upper_limit)
+            .expect("Failed to read line!");
+
+        match upper_limit.trim().parse::<u32>() {
+            Ok(num) => break num,
+            Err(_) => println!("Please enter a valid number!"),
+        }
+    };
+
+    let secret_number = rand::thread_rng().gen_range(1..=upper_limit);
+    let mut number_of_guesses: i32 = 0;
+
+    println!(
+        "The secret number has been set between 1 and {}!",
+        upper_limit
+    );
 
     loop {
         println!("Please input your guess.");
 
         let mut user_guess = String::new();
+        number_of_guesses += 1;
 
         io::stdin()
             .read_line(&mut user_guess)
@@ -31,6 +52,7 @@ fn main() {
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
                 println!("You win!");
+                println!("It took you {} attempts to guess!", number_of_guesses);
                 break;
             }
         }
